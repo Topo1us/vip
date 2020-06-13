d541ae147b3b0db66ccbdc074f430fd815835b02
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-06-14 02:30:51 +0300

    Update and rename vip_cod.py to hell.py

---
+++ hell.py
@@ -0,0 +1,14 @@
+import tkinter
+from tkinter import *
+ok=Tk()
+ok.configure(bg='black')
+ok.title('Termux Cod')
+lat=Label(ok,text='если у вас нет кода доступа\nобратитесь в тех.поддержку группы Termux Cod.',bg='black',fg='yellow')
+en=Entry(ok,show='*')
+but=Button(ok,text='войти',bg='black',fg='green')
+but_2=Button(ok,text='выйти',command=ok.quit,bg='black',fg='red')
+lat.pack()
+en.pack()
+but.pack()
+but_2.pack()
+ok.mainloop()
